f2164fdfa12b95abd64c0690bfa827381655f511
Isabella <isabella.tong.wu@gmail.com>
2020-04-15 14:26:18 -0400

    Lab06d

--- app/src/main/java/com/wuisabella/lab06/QuizActivity.java
+++ app/src/main/java/com/wuisabella/lab06/QuizActivity.java
@@ -2,6 +2,9 @@ package com.wuisabella.lab06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -11,21 +14,34 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
-    private TextView question;
-    private TextView score;
-    private TextView questionCount;
+    private TextView textViewQuestion;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
     private RadioGroup radioGroup;
     private RadioButton rb1;
     private RadioButton rb2;
     private RadioButton rb3;
     private Button confirm;
 
+    private ColorStateList defaultTextColor;
+
     private List<Question> questionList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
+
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +50,70 @@ public class QuizActivity extends AppCompatActivity {
 
         Gson gson = new Gson();
 
-        question = findViewById(R.id.text_question);
-        score = findViewById(R.id.text_score);
-        questionCount = findViewById(R.id.text_question_count);
+        textViewQuestion = findViewById(R.id.text_question);
+        textViewScore = findViewById(R.id.text_score);
+        textViewQuestionCount = findViewById(R.id.text_question_count);
         radioGroup = findViewById(R.id.radiogroup);
         rb1 = findViewById(R.id.radiobutton1);
         rb2 = findViewById(R.id.radiobutton2);
         rb3 = findViewById(R.id.radiobutton3);
         confirm = findViewById(R.id.button_confirm);
 
-        //questionList = new ArrayList<>();
-        //questionList = gson.fromJson(new FileReader("questions.json"), new TypeToken<ArrayList<Question>>(){}.getType());
+        defaultTextColor = rb1.getTextColors();
+
+        Type questionType = new TypeToken<ArrayList<Question>>(){}.getType();
+        questionList = gson.fromJson(read_json(), questionType);
+        Collections.shuffle(questionList);
+        questionCountTotal = questionList.size();
+
+        next_question();
 
     }
+
+    public String read_json() {
+        String json = null;
+        try {
+            InputStream is = QuizActivity.this.getAssets().open("questions.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    private void next_question(){
+        rb1.setTextColor(defaultTextColor);
+        rb2.setTextColor(defaultTextColor);
+        rb3.setTextColor(defaultTextColor);
+        radioGroup.clearCheck();
+
+        if(questionCounter < questionCountTotal){
+            currentQuestion = questionList.get(questionCounter);
+
+            textViewQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+
+            questionCounter++;
+            textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            answered = false;
+            confirm.setText("Confirm");
+        } else {
+            finish_quiz();
+        }
+    }
+
+    private void finish_quiz(){
+        finish();
+    }
+
+
 }
